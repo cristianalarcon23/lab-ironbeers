e1bcc8eb7fbe6cfe6ca2f8b4e649118966e86b26
Cristian Alarcon <cristian.alarcon.rodriguez@gmail.com>
2022-05-30 08:25:35 +0200

    1.0.1

--- app.js
+++ app.js
@@ -18,8 +18,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Add the route handlers here:
 
+
+
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beersFromApi => res.render('beers', {beersFromApi}))
+  .catch(error => console.log(error));
+})
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
